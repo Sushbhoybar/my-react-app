133e0233ebaa75b8c24b5767550e47a41b61149d
Sushbhoybar <sushilbhoybar99@gmail.com>
2025-01-20 00:20:34 +0530

    Update backend API URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({ baseURL: "https://my-react-app-backend.onrender.com" });
+
 
 export const getCategories = () => API.get("/categories");
 export const getProducts = () => API.get("/products");
